b9e904d5a4e59c3de9061d4b44c72e5db6b20f16
GeorgeSJ1869 <george_miao@qq.com>
2025-02-19 05:10:44 +0800

    Implemented level-6 and related error handling

--- src/main/java/mainstructure/taskmanager/TaskList.java
+++ src/main/java/mainstructure/taskmanager/TaskList.java
@@ -1,13 +1,14 @@
 package mainstructure.taskmanager;
 
 import mainstructure.UI;
+import java.util.ArrayList;
 
 public class TaskList {
-    protected Task[] taskList;
+    protected ArrayList<Task> taskList;
     protected int taskCount;
 
     public TaskList(){
-        taskList = new Task[100];
+        taskList = new ArrayList<Task>();
         taskCount = 0;
     }
 
@@ -16,21 +17,34 @@ public class TaskList {
     }
 
     public void addTask(String description){
-        taskList[taskCount] = new Todo(description);
+        taskList.add(new Todo(description));
         taskCount++;
         UI.printAddTaskMessage(showTask(taskCount), taskCount);
     }
 
     public void addTask(String description, String deadline){
-        taskList[taskCount] = new Deadline(description, deadline);
+        taskList.add(new Deadline(description, deadline));
         taskCount++;
         UI.printAddTaskMessage(showTask(taskCount), taskCount);
     }
 
     public void addTask(String description, String startTime, String endTime){
-        taskList[taskCount] = new Event(description, startTime, endTime);
+        taskList.add(new Event(description, startTime, endTime));
         taskCount++;
         UI.printAddTaskMessage(showTask(taskCount), taskCount);
+    }
+
+    public void deleteTask(int index){
+        try {
+            String target = showTask(index);
+            taskList.remove(index - 1);
+            taskCount--;
+            UI.print("Task deleted:\n"
+                    + target + "\n"
+                    + "Task count now: " + taskCount + '\n');
+        } catch (IndexOutOfBoundsException e){
+            UI.print("Invalid task number! Type " + '"' + "list" + '"' + " to view the list of tasks\n");
+        }
     }
 
     public void mark(int taskIndex, boolean status){
@@ -38,13 +52,13 @@ public class TaskList {
             UI.print("Invalid task number! Type " + '"' + "list" + '"' + " to view the list of tasks\n");
             return;
         }
-        taskList[taskIndex - 1].mark(status);
+        taskList.get(taskIndex - 1).mark(status);
         UI.print("This task is marked as done:\n"
                     + showTask(taskIndex) + '\n');
     }
 
     public String showTask(int taskIndex){
-        return taskList[taskIndex - 1].toString();
+        return taskList.get(taskIndex - 1).toString();
     }
 
     public String showFullList(){
@@ -53,7 +67,7 @@ public class TaskList {
         } else {
             String list = "Here is the list of all tasks:\n";
             for (int i = 0; i < taskCount; i++){
-                list += (Integer.toString(i + 1) + ". " + taskList[i].toString() + "\n");
+                list += (Integer.toString(i + 1) + ". " + taskList.get(i).toString() + "\n");
             }
             return list;
         }
