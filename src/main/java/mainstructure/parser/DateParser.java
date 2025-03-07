c0b62d7aaa559d39cbbb30df1e2c701eabb6e1c6
GeorgeSJ1869 <george_miao@qq.com>
2025-03-07 17:40:48 +0800

    Updated JavaDoc for DateParser

--- src/main/java/mainstructure/parser/DateParser.java
+++ src/main/java/mainstructure/parser/DateParser.java
@@ -3,7 +3,16 @@ package mainstructure.parser;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Contains a method to parse the date of a given input.
+ */
 public class DateParser {
+    /**
+     * Parses the date of a given input.
+     * @param input the String where the date is parsed.
+     * @return a <code>LocalDate</code> class.
+     * @throws DateTimeParseException when no dates can be parsed.
+     */
     public static LocalDate parse(String input) throws DateTimeParseException {
         LocalDate date;
         String processed = input.replaceAll("\\D", " ").trim().replaceAll(" +", "-");
